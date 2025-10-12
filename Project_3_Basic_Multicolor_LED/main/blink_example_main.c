4152f2243c7f16ca2a3b061dce119a8c4259a456
sgaleanoca <sgaleanoca@unal.edu.co>
2025-10-12 18:27:24 -0500

    Se crea la librería del led RGB y el funcionamiento .c, cambiando el main.c

--- Project_3_Basic_Multicolor_LED/main/blink_example_main.c
+++ Project_3_Basic_Multicolor_LED/main/blink_example_main.c
@@ -5,7 +5,38 @@
 
 // --- Definición de los pines ---
 // Aquí asignamos un nombre a cada número de pin que estamos usando.
-#define RED_PIN   GPIO_NUM_5 // El pin Rojo (R) del LED está conectado al GPIO 5.
+#include <stdio.h>
+#include "rgb_led.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    rgb_led_t my_led = {
+        .pin_r = 5,  // Pines donde conectas R, G, B
+        .pin_g = 19,
+        .pin_b = 23,
+        .channel_r = LEDC_CHANNEL_0,
+        .channel_g = LEDC_CHANNEL_1,
+        .channel_b = LEDC_CHANNEL_2,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_sel = LEDC_TIMER_0
+    };
+
+    rgb_led_init(&my_led);
+
+    while (1) {
+        rgb_led_set_color(&my_led, 255, 0, 0);  // Rojo
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        rgb_led_set_color(&my_led, 0, 255, 0);  // Verde
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        rgb_led_set_color(&my_led, 0, 0, 255);  // Azul
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        rgb_led_set_color(&my_led, 255, 255, 0); // Amarillo
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        rgb_led_set_color(&my_led, 0, 0, 0); // Apagar
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 #define GREEN_PIN GPIO_NUM_19 // El pin Verde (G) del LED está conectado al GPIO 19.
 #define BLUE_PIN  GPIO_NUM_23 // El pin Azul (B) del LED está conectado al GPIO 23.
 #define BLINK_DELAY_MS 1000   // Definimos el tiempo de parpadeo en milisegundos (1000 ms = 1 segundo).
